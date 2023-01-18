c7414d600ba272902c33438abea390e4e0c2bb1b
Nazar Niphone <74293442+nazar1ua@users.noreply.github.com>
2023-01-18 20:33:13 +0200

    Збільшено тривалість анімації

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -8,10 +8,10 @@ onMounted(() => {
 </script>
 
 <template>
-  <header class="bg-slate-800 text-slate-50 fixed top-0 left-0 right-0 duration-300 transition-spacing" :class="!y ? 'py-4' : 'py-2' ">
+  <header class="bg-slate-800 text-slate-50 fixed top-0 left-0 right-0 duration-500 transition-spacing" :class="!y ? 'py-4' : 'py-2' ">
     <div class="px-6 lg:px-8 flex items-center gap-6">
       <NuxtLink to="/">
-        <TheLogo class="w-auto transition-height duration-300" :class="!y ? 'h-8' : 'h-6'" />
+        <TheLogo class="w-auto transition-height duration-500" :class="!y ? 'h-8' : 'h-6'" />
       </NuxtLink>
       <nav class="flex items-center gap-4">
         <NuxtLink to="/gems" class="whitespace-nowrap border-b pb-[1px]" active-class="font-semibold !border-b-2 !pb-0">Пам'ятні перлини</NuxtLink>
