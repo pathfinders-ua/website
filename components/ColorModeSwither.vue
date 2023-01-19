58a7a78dfdda8727e1259f119a9b8e46a8a25889
Nazar Niphone <74293442+nazar1ua@users.noreply.github.com>
2023-01-19 20:42:07 +0200

    Виправлено помилку при зміні теми

--- components/ColorModeSwither.vue
+++ components/ColorModeSwither.vue
@@ -1,19 +1,23 @@
 <script setup lang="ts">
-import {SunIcon, MoonIcon} from '@heroicons/vue/24/outline/index'
+import { SunIcon, MoonIcon } from '@heroicons/vue/24/outline/index'
 
 withDefaults(defineProps<{
   y: number
 }>(), {
-  y: 0
+  y: 0,
 })
 
 const colorMode = useColorMode()
 
-const switchColorMode = () => {
+const temporallyDisableTransitions = () => {
   document.documentElement.classList.add('[&_*]:!transition-none')
   setTimeout(() => {
     document.documentElement.classList.remove('[&_*]:!transition-none')
   }, 0)
+}
+
+const switchColorMode = () => {
+  temporallyDisableTransitions()
 
   if (colorMode.preference === 'system') {
     colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
@@ -21,14 +25,13 @@ const switchColorMode = () => {
     colorMode.preference = 'system'
   }
 }
-
-const Icon = computed(() => colorMode.value === 'dark' ? MoonIcon : SunIcon)
 </script>
 
 <template>
   <div class="flex items-center">
     <button @click="switchColorMode" class="p-1.5 rounded-full transition-colors hover:bg-slate-700 dark:hover:bg-zinc-700">
-      <component :is="Icon" class="transition-sizes duration-500" :class="!y ? 'h-6 w-6' : 'h-5 w-5'" height="24" width="24" />
+      <SunIcon class="hidden transition-sizes duration-500 dark:block" :class="!y ? 'h-6 w-6' : 'h-5 w-5'" height="24" width="24" />
+      <MoonIcon class="block transition-sizes duration-500 dark:hidden" :class="!y ? 'h-6 w-6' : 'h-5 w-5'" height="24" width="24" />
     </button>
   </div>
 </template>
