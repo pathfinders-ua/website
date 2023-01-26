8738f23e350c8beb199b6341b66d390b8d0142b2
Nazar Niphone <74293442+nazar1ua@users.noreply.github.com>
2023-01-26 16:03:09 +0200

    Видалено журналювання

--- middleware/slash.global.ts
+++ middleware/slash.global.ts
@@ -1,5 +1,4 @@
 export default defineNuxtRouteMiddleware(to => {
-  console.log(to.fullPath)
   if (to.fullPath.substring(to.fullPath.length - 1) === '/') {
     navigateTo(to.fullPath.substring(0, to.fullPath.length - 1))
   }
