eb3c1b6538c60f1ee2711102aba9fc97b3f43a27
Nazar Niphone <74293442+nazar1ua@users.noreply.github.com>
2023-03-14 21:51:21 +0200

    Видалено лишній пробіл

--- middleware/slash.global.ts
+++ middleware/slash.global.ts
@@ -1,5 +1,5 @@
 export default defineNuxtRouteMiddleware(to => {
   if (to.fullPath !== '/' && to.fullPath.substring(to.fullPath.length - 1) === '/') {
-    return  navigateTo(to.fullPath.substring(0, to.fullPath.length - 1))
+    return navigateTo(to.fullPath.substring(0, to.fullPath.length - 1))
   }
 })
