486daaa9c428b053835560e8e2091d71fbf9f1c0
Nazar Niphone <74293442+nazar1ua@users.noreply.github.com>
2023-03-27 13:44:42 +0300

    Додано попередження про іспит

--- utils/getCurrentGem.ts
+++ utils/getCurrentGem.ts
@@ -2,7 +2,14 @@ export const getCurrentGem = async () => {
   const now = useNow()
   const quarter = computed(() => Math.floor((now.value.getMonth() + 3) / 3))
   const week = getWeekNumber(new Date(), quarter.value)
-  const {data: currentQuarter} = await useAsyncData('current-gem', () => queryContent('/gems/q' + quarter.value).findOne())
+  if (week === 13) return <CurrentGem>{
+    text: 'Іспит',
+    location: '',
+    quarter: quarter.value,
+    week: week,
+  }
+
+  const { data: currentQuarter } = await useAsyncData('current-gem', () => queryContent('/gems/q' + quarter.value).findOne())
 
   const gem: CurrentGem = {
     text: currentQuarter.value?.gems?.[week - 1]?.text,
